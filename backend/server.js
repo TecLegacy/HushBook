f484836af28d2fbe49ea44af864a65f31a9de894
TecLegacy <99929931+TecLegacy@users.noreply.github.com>
2022-06-17 04:05:37 +0530

    Hush Book inital Env

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log('Our Secrete Server is Running 😎');
+});
+app.use('/kesh', (req, res) => {
+  res.send('its working');
+});
