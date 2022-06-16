b8acb2069b245cc6cbd2147a48b1d3e9895ef28c
TecLegacy <99929931+TecLegacy@users.noreply.github.com>
2022-06-17 04:33:40 +0530

    API CRUD operations

--- backend/routes/secreteRoutes.js
+++ backend/routes/secreteRoutes.js
@@ -1,9 +1,35 @@
+//CRUD operation with HUSH-> user Secrets
 const express = require('express');
 const router = express.Router();
 
+//@description GatherAll  hush
+//@routes get->secrete/hush/
+//@acces Public
 router.get('/', (req, res) => {
   res.status(200).json({
-    message: 'Getting all Secretes',
+    message: 'Getting all Secretes  😊',
+  });
+});
+router.post('/', (req, res) => {
+  res.status(200).json({
+    message: 'Creating Hush!! 😎',
+  });
+});
+
+//@description update hush
+//@routes put->secrete/hush/ID
+//@acces Private
+router.put('/:id', (req, res) => {
+  res.status(200).json({
+    message: 'Making Changes to Hush 😇',
+  });
+});
+//@description delete hush
+//@routes delete->secrete/hush/ID
+//@acces Private
+router.delete('/:id', (req, res) => {
+  res.status(200).json({
+    message: 'Deleting all Hush 😔',
   });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Our Secrete Server is Running 😎');
 });
-app.use('/kesh', (req, res) => {
-  res.send('its working');
-});
+
 app.use('/secrete/hush', require('./routes/secreteRoutes'));
