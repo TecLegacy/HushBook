68683da7ce5c0dbf09867d3673cdc09abb02267b
TecLegacy <99929931+TecLegacy@users.noreply.github.com>
2022-06-18 01:53:39 +0530

    express asyncHandler and controller steup with ErrorMiddlerware

--- backend/controller/secreteOperations.js
+++ backend/controller/secreteOperations.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
-const get = (req, res) => {
+const expressAsyncHandler = require('express-async-handler');
+//@descption Get hush
+//@routes Get/secrete/hush
+//@access private
+const get = expressAsyncHandler(async (req, res) => {
   res.status(400).json({
     message: 'Getting all Secretes  😊',
   });
-};
+});
 
-const post = (req, res) => {
+//@descption Create hush
+//@routes POST/secrete/hush
+//@access private
+const post = expressAsyncHandler(async (req, res) => {
   console.log(req.body.name);
   if (!req.body.name) {
     res.status(200);
@@ -17,17 +23,24 @@ const post = (req, res) => {
     message: 'Creating Hush! 😎',
     name: data,
   });
-};
+});
 
-const put = (req, res) => {
+//@descption update hush
+//@routes PUT/secrete/hush
+//@access private
+const put = expressAsyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Making Changes to Hush 😇',
   });
-};
+});
 
-const remove = (req, res) => {
+//@descption delete hush
+//@routes Delete/secrete/hush
+//@access private
+const remove = expressAsyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Deleting all Hush 😔',
   });
-};
+});
+
 module.exports = { get, remove, put, post };
